befcedb5bddeb66aceb9060940b3002745e309be
awais <macheema3004@gmail.com>
2026-08-20 21:20:49 +0200

    Add Phase 2 security tests; align role authorities to uppercase convention

--- silo/build.gradle.kts
+++ silo/build.gradle.kts
@@ -15,4 +15,6 @@ dependencies {
     implementation(libs.bundles.spring.web)
     implementation(local.bundles.spring.security)
 
+    testImplementation(local.spring.security.test)
+    testImplementation(local.spring.boot.webmvc.test)
 }
