c9ce977cc7e8efc49ab6f14ed414189b3d8da313
awais <macheema3004@gmail.com>
2026-08-27 13:42:39 +0200

    Use Spring interceptor component for adding token to Authorization header.

--- silo/src/main/kotlin/io/github/siloverse/auth/config/ClientConfiguration.kt
+++ silo/src/main/kotlin/io/github/siloverse/auth/config/ClientConfiguration.kt
@@ -5,8 +5,8 @@ import io.github.siloverse.auth.config.settings.UserSiloClientSettings
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor
 import org.springframework.web.client.RestClient
 
 @Configuration
@@ -16,7 +16,7 @@ class ClientConfiguration {
     fun keycloakAdminRestClient(
         builder: RestClient.Builder,
         keycloakClientSettings: KeycloakClientSettings,
-        clientManager: OAuth2AuthorizedClientManager
+        clientManager: OAuth2AuthorizedClientManager,
     ): RestClient {
         return builder.baseUrl(keycloakClientSettings.baseUrl)
             .requestInterceptor(oauth2ClientCredentialsInterceptor(clientManager))
@@ -37,16 +37,12 @@ class ClientConfiguration {
 
 
     private fun oauth2ClientCredentialsInterceptor(
-        clientManager: OAuth2AuthorizedClientManager
+        clientManager: OAuth2AuthorizedClientManager,
     ): ClientHttpRequestInterceptor {
-        return ClientHttpRequestInterceptor { request, body, execution ->
-            val authorized = clientManager.authorize(
-                OAuth2AuthorizeRequest.withClientRegistrationId("auth-silo")
-                    .principal("auth-silo")
-                    .build()
-            ) ?: error("could not obtain client-credentials token for 'auth-silo'")
-            request.headers.setBearerAuth(authorized.accessToken.tokenValue)
-            execution.execute(request, body)
+        return OAuth2ClientHttpRequestInterceptor(clientManager).apply {
+            setClientRegistrationIdResolver {
+                "auth-silo"
+            }
         }
     }
 }
